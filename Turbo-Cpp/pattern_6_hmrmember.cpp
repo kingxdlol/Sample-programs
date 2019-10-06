2d8bff1369af794bd46258c146f5b917ade223f1
hmrmember <43930219+hmrmember@users.noreply.github.com>
2019-10-06 19:16:25 +0530

    Pattern 6

---
+++ Turbo-Cpp/pattern_6_hmrmember.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+void main() {
+clrscr();
+int i, j;
+char ch='A';
+for(i=0; i<5; i++) {
+for(j=0; j<=i; j++) {
+cout<<ch<<" ";
+ch++; }
+cout<<"
+\n";
+}
+getch(); }
