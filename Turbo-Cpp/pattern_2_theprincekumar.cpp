7919b300bc052522498295d30daa27b81387c6b9
theprincekumar <43923485+theprincekumar@users.noreply.github.com>
2019-10-06 18:56:46 +0530

    Pattern 2

---
+++ Turbo-Cpp/pattern_2_theprincekumar.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+void main(){
+clrscr();
+int i, j, k=8;
+for(i=0; i<5; i++){
+for(j=0; j<k; j++){
+cout<<" "; }
+k=k
+-2;
+for(j=0; j<=i; j++){
+cout<<"* "; }
+cout<<"
+\n";
+}}
