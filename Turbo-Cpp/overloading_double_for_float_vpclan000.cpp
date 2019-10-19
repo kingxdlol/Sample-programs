e4b256626be926b4d2dec875919f5b7a7f7072ee
vpclan000 <56772766+vpclan000@users.noreply.github.com>
2019-10-20 01:37:16 +0530

    Overloading using double for Float

---
+++ Turbo-Cpp/overloading_double_for_float_vpclan000.cpp
@@ -0,0 +1,19 @@
+#include<iostream.h>
+#include<conio.h>
+void show(int a){
+cout<<"int"<<a;}
+void show(long a){
+cout<<"long"<<a;}
+void show(double a){
+cout<<"double"<<a;}
+void show(char a){
+cout<<"char"<<a;}
+void show(unsigned int a){
+cout<<"un int"<<a;}
+void main() {
+clrscr();
+float i=9.0;
+cout<<"
+\nCASE 1: ";
+show(i);
+getch(); }
