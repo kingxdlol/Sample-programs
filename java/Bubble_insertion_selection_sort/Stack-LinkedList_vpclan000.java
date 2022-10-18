bb445558917d75c73e0a04b210b2bc38f9fffa60
vpclan000 <56772766+vpclan000@users.noreply.github.com>
2022-10-19 01:17:38 +0530

    stack using a linked list

---
+++ java/Bubble_insertion_selection_sort/Stack-LinkedList_vpclan000.java
@@ -0,0 +1,68 @@
+public class Stack {
+    private Node top;
+    private int size;
+
+    public Stack() {
+        top = null;
+        size = 0;
+    }
+
+    public void push(int data) {
+        Node newNode = new Node(data);
+        newNode.next = top;
+        top = newNode;
+        size++;
+    }
+
+    public int pop() {
+        if (top == null) {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        int data = top.data;
+        top = top.next;
+        size--;
+        return data;
+    }
+
+    public int peek() {
+        if (top == null) {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        return top.data;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public void printStack() {
+        Node temp = top;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.printStack();
+        System.out.println("Size of stack: " + stack.size());
+        System.out.println("Top element: " + stack.peek());
+        System.out.println("Popped element: " + stack.pop());
+        System.out.println("Top element: " + stack.peek());
+        System.out.println("Size of stack: " + stack.size());
+        System.out.println("Is stack empty: " + stack.isEmpty());
+    }
+}
